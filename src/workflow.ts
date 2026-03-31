5fffeb1b2f323578eb99ce2275339816d9cdd4a4
Chris <chris@crisp.chat>
2026-03-31 22:27:34 +0200

    Feat: Workflow method

---
+++ src/workflow.ts
@@ -0,0 +1,15 @@
+import { CrispClass as Crisp } from "./index";
+
+export default class CrispWorkflow {
+  private parent: Crisp;
+
+  constructor(crisp: Crisp) {
+    this.parent = crisp;
+  }
+
+  run(name: string) {
+    this.parent.createSingletonIfNecessary();
+
+    window.$crisp.push(["do", "bot:scenario:run", [name]]);
+  }
+}
